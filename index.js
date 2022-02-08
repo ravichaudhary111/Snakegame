972a0fd4326a5d7bd862a04900fbcb689275c784
ravichaudhary111 <86255362+ravichaudhary111@users.noreply.github.com>
2022-02-08 22:23:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function gameEngine() {
         if (score > hiscoreval) {
             hiscoreval = score;
             localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
-            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
+            hiscoreBox.innerHTML = "HighScore: " + hiscoreval;
         }
         scoreBox.innerHTML = "Score: " + score;
         snakeArr.unshift({ x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y });
